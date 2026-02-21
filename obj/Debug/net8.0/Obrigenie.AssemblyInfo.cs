3f8f25cbf11144cc0c6a1d0b1949795dfeaa83c2
johan <166138951+DragX02@users.noreply.github.com>
2026-02-21 14:07:43 +0100

    reglage secur

--- obj/Debug/net8.0/Obrigenie.AssemblyInfo.cs
+++ obj/Debug/net8.0/Obrigenie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Obrigenie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2056c351633607634ac633bb6713da300ad13dfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dcf447ffbadb18585f730bc5a1fc3f9dbbad894")]
 [assembly: System.Reflection.AssemblyProductAttribute("Obrigenie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Obrigenie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
